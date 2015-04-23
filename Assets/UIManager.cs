6ef1cf3a21ea020f6d31f2a8218db5ffdf77391d
ryan <nixamas@gmail.com>
2015-04-22 22:13:03 -0500

    Added opening menu scene

---
+++ Assets/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame() {
+		Application.LoadLevel ("GameBoardScene");
+	}
+
+	public void LoadTutorial () {
+		Debug.Log ("TODO make tutorial");
+	}
+
+	public void LoadSettings () {
+		Debug.Log ("TODO make settings page");
+	}
+}
